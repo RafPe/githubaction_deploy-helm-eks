37c6c7fc328c0b3bde5e2618257dcad5b409fbb0
Rafal Pieniazek <rafal.pieniazek@persgroep.net>
2021-10-01 00:01:02 +0200

    add(setup): initial commit

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Login to Kubernetes Cluster.
+if [ -n "$CLUSTER_ROLE_ARN" ]; then
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} \
+        --role-arn=${CLUSTER_ROLE_ARN}
+else
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} 
+fi
+
+# Helm Deployment
+DEPS_UPDATE_COMMAND="helm dependency update ${DEPLOY_CHART_PATH}"
+UPGRADE_COMMAND="helm upgrade --install --timeout ${TIMEOUT}"
+if [ -n "$DEPLOY_ATOMIC" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --atomic"
+fi
+for config_file in ${DEPLOY_CONFIG_FILES//,/ }
+do
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
+done
+if [ -n "$DEPLOY_NAMESPACE" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -n ${DEPLOY_NAMESPACE}"
+fi
+if [ -n "$DEPLOY_VALUES" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
+fi
+UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
+echo "Executing: ${DEPS_UPDATE_COMMAND}"
+${DEPS_UPDATE_COMMAND}
+echo "Executing: ${UPGRADE_COMMAND}"
+${UPGRADE_COMMAND}
